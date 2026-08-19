01bd3c6e17a588aad375e22c3f17414afc0490f4
dallenlee <dallenjap123@gmail.com>
2026-08-20 00:26:25 +0800

    first test push

---
+++ main.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    	return a + b
+
+
+def main():
+	numbers = [1, 2, 3, 4, 5]
+	print("Numbers:", numbers)
+	print("Sum:", sum(numbers))
+	print("2 + 3 =", add(2, 3))
+
+
+if __name__ == "__main__":
+	main()
